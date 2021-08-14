c34970e528c33f3b2b76460331c0a1de2b8ce49d
Diego Melo <diego_14melo@hotmail.com>
2021-08-14 12:00:53 -0500

    Navbar mobile

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -6,8 +6,6 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <nav class="navbar" role="navigation" aria-label="main navigation">
-            
-
             <div id="navbar-element" id='navbar-menu' class="navbar-menu">
                 <div class="navbar-start">
                     <a class="navbar-item">
@@ -21,12 +19,13 @@ class Header extends HTMLElement {
                     <a class="navbar-item">
                         Recipe list
                     </a>
-                </div>
-
-                <div class="navbar-brand">
+                    <div class="navbar-brand">
                 <a class="navbar-item" href="https://bulma.io">
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
                 </a>
+                </div>
+
+                
 
                 <a role="button" id='navbar-burguer' class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-element" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
                     <span aria-hidden="true"></span>
