25ed1ffba507ed9def722fa0efa4147b4741c60e
Diego Melo <diego_14melo@hotmail.com>
2021-08-14 12:26:00 -0500

    Alineación de input en mobile

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -6,34 +6,37 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div id="navbar-element" id='navbar-menu' class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item">
-                    Home
-                    </a>
-
-                    <a class="navbar-item">
-                    About us
-                    </a>
-
-                    <a class="navbar-item">
-                        Recipe list
-                    </a>
-                    <div class="navbar-brand">
+            <div class="navbar-brand">
                 <a class="navbar-item" href="https://bulma.io">
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
                 </a>
-                </div>
-
-                
-
-                <a role="button" id='navbar-burguer' class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-element" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
+            
+                <a role="button" class="navbar-burger " aria-label="menu" aria-expanded="false" data-target="navbar-menu__collapsed" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
+        
+            <div id="navbar-menu__collapsed" class="navbar-menu navbar-start ">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+            
+                    <a class="navbar-item">
+                        About us
+                    </a>
 
+                    <a class="navbar-item">
+                        Recipe list
+                    </a>
+                </div>
+
+                <a class="navbar-item centered-logo" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                </a>
+            
                 <div class="navbar-end">
                     <div class="navbar-item">
                         <a class="navbar-item">
