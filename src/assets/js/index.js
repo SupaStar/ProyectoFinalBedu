d7c795ea72ef3980f19aabae2ff963e22ad22d3d
Obed Martinez <martinezobednoe@gmail.com>
2021-08-15 14:23:34 -0500

    Funcion si se ingresa a recipe, sin un id se muestre una receta aleatoria

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -15,7 +15,5 @@ import bg from '../img/background4-about.jpg';
 
 export { mainImage, logo, logo_long };
 
-import './carousel.js';
-
 // document.querySelector('.about').style.background =  `url(${bg}) no-repeat fixed 0 0 / cover`;
 
