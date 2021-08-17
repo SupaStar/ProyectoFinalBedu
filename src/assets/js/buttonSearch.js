2bab4e82b0a3225fac99f81bb26e1507130fac34
Obed Martinez <martinezobednoe@gmail.com>
2021-08-17 00:08:38 -0500

    Prevent default

--- src/assets/js/buttonSearch.js
+++ src/assets/js/buttonSearch.js
@@ -1,5 +1,6 @@
 let buttonSearch = document.getElementById('buscar');
-buttonSearch.addEventListener('click', () => {
+buttonSearch.addEventListener('click', (e) => {
+    e.preventDefault();
     let platillo=document.getElementById('platillo').value;
     window.location=`/results.html?busqueda=${platillo}`;
 })
