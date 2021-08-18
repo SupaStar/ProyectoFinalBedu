ed9dfee8bc64f6129e56051f797069adbc06193d
Diego Melo <diego_14melo@hotmail.com>
2021-08-17 20:54:27 -0500

    Imágenes en about

--- src/assets/js/about_us.js
+++ src/assets/js/about_us.js
@@ -1,3 +1,8 @@
 import '../css/about_us.css';
 import 'bulma/css/bulma.css';
 import '../img/background1-about.jpg'
+import '../img/about_obed.jpg';
+import '../img/about_diego.jpg';
+import '../img/about_vjv.jpg';
+import '../img/about_rogelio.jpg';
+import '../img/about_sergio.jpg';
